26cad1b295d9930de7ea1321f7d7c5f965ded123
Felipe Moreira da Cruz <piradous@gmail.com>
2023-09-15 05:50:27 -0300

    feat: req 01 - function atoresQueInterpretaramSiProprios

--- src/main/java/com/trybe/consultafilmes/Consultas.java
+++ src/main/java/com/trybe/consultafilmes/Consultas.java
@@ -2,13 +2,16 @@ package com.trybe.consultafilmes;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptyMap;
-import static java.util.Collections.emptySet;
-
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+/**
+ * Classe para realizar consultas na classe filmes.
+ */
 public class Consultas {
 
   private final Collection<Filme> filmes;
@@ -18,15 +21,31 @@ public class Consultas {
   }
 
   /**
-   * Consulta 1: a partir da coleção de filmes desta classe, este método retorna o conjunto
-   * de atores que interpretaram a si próprios em pelo menos um desses filmes.
+   * Consulta 1: a partir da coleção de filmes desta classe, este método retorna o conjunto de
+   * atores que interpretaram a si próprios em pelo menos um desses filmes.
    *
-   * <p>Considera-se "atores que interpretaram a si próprios" aqueles que têm o seu nome como
-   * uma das chaves do Map `atoresPorPersonagem` e também como um dos itens pertencentes ao
-   * conjunto associado a esta mesma chave.</p>
+   * <p>
+   * Considera-se "atores que interpretaram a si próprios" aqueles que têm o seu nome como uma das
+   * chaves do Map `atoresPorPersonagem` e também como um dos itens pertencentes ao conjunto
+   * associado a esta mesma chave.
+   * </p>
    */
   public Set<String> atoresQueInterpretaramSiProprios() {
-    return emptySet(); // TODO: Implementar.
+    Set<String> listaAtoresQueInterpretaramSiProprios = new HashSet<String>();
+
+    for (Filme filme : filmes) {
+      Map<String, Set<String>> atoresPorPersonagem = filme.atoresPorPersonagem;
+      for (Map.Entry<String, Set<String>> entrada : atoresPorPersonagem.entrySet()) {
+        String personagem = entrada.getKey();
+        Set<String> atores = entrada.getValue();
+        Set<String> atorQueInterpretouSiMesmo =
+            atores.stream().filter(ator -> ator.equals(personagem)).collect(Collectors.toSet());
+
+        listaAtoresQueInterpretaramSiProprios.addAll(atorQueInterpretouSiMesmo);
+
+      }
+    }
+    return listaAtoresQueInterpretaramSiProprios;
   }
 
   /**
@@ -34,9 +53,11 @@ public class Consultas {
    * que atuaram em pelo menos um filme de um determinado diretor. A lista retornada está disposta
    * em ordem alfabética.
    *
-   * <p>Considera-se que um ator tenha atuado em um filme de um determinado diretor se ele tem o
-   * seu nome como um dos itens do campo `atores`, ao mesmo tempo em que o diretor em questão
-   * tem o seu nome como um dos itens do campo `diretores` do mesmo filme.</p>
+   * <p>
+   * Considera-se que um ator tenha atuado em um filme de um determinado diretor se ele tem o seu
+   * nome como um dos itens do campo `atores`, ao mesmo tempo em que o diretor em questão tem o seu
+   * nome como um dos itens do campo `diretores` do mesmo filme.
+   * </p>
    */
   public List<String> atoresQueAtuaramEmFilmesDoDiretorEmOrdemAlfabetica(String diretor) {
     return emptyList(); // TODO: Implementar.
@@ -47,8 +68,10 @@ public class Consultas {
    * em que pelo menos um dos diretores tenha atuado. A lista retornada está disposta em ordem de
    * lançamento, com os filmes mais recentes no início.
    *
-   * <p>Considera-se "filmes em que pelo menos um dos diretores tenha atuado" aqueles em que
-   * pelo menos um dos itens do campo `diretores` também é um item do campo `atores`.</p>
+   * <p>
+   * Considera-se "filmes em que pelo menos um dos diretores tenha atuado" aqueles em que pelo menos
+   * um dos itens do campo `diretores` também é um item do campo `atores`.
+   * </p>
    */
   public List<Filme> filmesEmQuePeloMenosUmDiretorAtuouMaisRecentesPrimeiro() {
     return emptyList(); // TODO: Implementar.
@@ -58,8 +81,10 @@ public class Consultas {
    * Consulta 4: a partir da coleção de filmes desta classe, este método retorna um Map contendo
    * todos os filmes lançados em um determinado ano agrupados por categoria.
    *
-   * <p>Cada chave do Map representa uma categoria, enquanto cada valor representa o
-   * conjunto de filmes que se encaixam na categoria da chave correspondente.</p>
+   * <p>
+   * Cada chave do Map representa uma categoria, enquanto cada valor representa o conjunto de filmes
+   * que se encaixam na categoria da chave correspondente.
+   * </p>
    */
   public Map<String, Set<Filme>> filmesLancadosNoAnoAgrupadosPorCategoria(int ano) {
     return emptyMap(); // TODO: Implementar (bônus).
